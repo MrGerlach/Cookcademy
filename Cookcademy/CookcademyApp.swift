4323eb971a24302a27478d0413c84f086bce61be
Mr. Gerlach <mgerl@tlen.pl>
2022-07-21 17:54:53 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Michał Gerlach on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
